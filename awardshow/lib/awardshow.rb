2d1d5255fc92a8286c32d0d2af8a8ff6c0a7a658
KellyMarcilliat <kelly.marcilliat@gmail.com>
2018-06-05 20:23:49 -0600

    WIP - iteration 3 complete

--- awardshow/lib/awardshow.rb
+++ awardshow/lib/awardshow.rb
@@ -4,10 +4,46 @@ require 'pry'
 class AwardShow
 
 attr_reader   :year,
-              :nominees
+              :nominees#,
+              # :nominated_movies
   def initialize(year)
     @year = year
     @nominees = []
+    # @nominated_movies = []
   end
+
+  def add_nominee(nominee)
+    @nominees << nominee
+    # binding.pry
+  end
+
+  def nominated_movies
+    @nominees.map do |each|
+      each.movie
+    end
+  end
+
+  def average_salary
+    @salaries = []
+    @nominees.map do |each|
+      @salaries << each.actor.net_worth.to_i
+    end
+    return @salaries.sum/@salaries.count
+    # binding.pry
+  end
+
+  # def add_nominated_movie
+  #   @nominated_movies << nominee.movie
+  #   return @nominated_movies
+  #   binding.pry
+  # end
+
+  # def add_nominated_movie
+  #   @nominees.map do |each|
+  #     @nominated_movies << each.movie
+  #     # return @nominated_movies
+  #   end
+  #   binding.pry
+  # end
 
 end
--- awardshow/test/awardshow_test.rb
+++ awardshow/test/awardshow_test.rb
@@ -4,6 +4,7 @@ require 'minitest/pride'
 require './lib/awardshow'
 require '../actor/lib/actor'
 require '../movie/lib/movie'
+require '../nominee/lib/nominee'
 
 class AwardShowTest < Minitest::Test
 
@@ -24,7 +25,81 @@ class AwardShowTest < Minitest::Test
     assert_equal [], show.nominees
   end
 
-  # +show.nominees
-  # +#=> []
+  def test_awardshow_nominee_array_remains_empty_with_new_actors_nominees_movies
+    show = AwardShow.new(2017)
+    actress_1 = Actor.new("Jennifer Lawrence", 1990, "110000000")
+    movie_1 = Movie.new("Hunger Games", 2012)
+    nominee_1 = Nominee.new(actress_1, movie_1)
+    assert_equal [], show.nominees
 
+    actress_2 = Actor.new("Brie Larson", 1989, "10000000")
+    movie_2 = Movie.new("Room", 2015)
+    nominee_2 = Nominee.new(actress_2, movie_2)
+    assert_equal [], show.nominees
+  end
+
+  def test_nominee_can_be_added_to_nominees_array
+    show = AwardShow.new(2018)
+    actress_1 = Actor.new("Jennifer Lawrence", 1990, "110000000")
+    movie_1 = Movie.new("Hunger Games", 2012)
+    nominee_1 = Nominee.new(actress_1, movie_1)
+    show.add_nominee(nominee_1)
+    assert_equal [nominee_1], show.nominees
+  end
+
+  def test_nominees_array_can_hold_additional_nominees
+    show = AwardShow.new(2018)
+    actress_1 = Actor.new("Jennifer Lawrence", 1990, "110000000")
+    movie_1 = Movie.new("Hunger Games", 2012)
+    nominee_1 = Nominee.new(actress_1, movie_1)
+    show.add_nominee(nominee_1)
+
+    actress_2 = Actor.new("Brie Larson", 1989, "10000000")
+    movie_2 = Movie.new("Room", 2015)
+    nominee_2 = Nominee.new(actress_2, movie_2)
+    show.add_nominee(nominee_2)
+    assert_equal [nominee_1, nominee_2], show.nominees
+  end
+
+  def test_it_can_show_nominated_movies
+    show = AwardShow.new(2018)
+
+    actress_1 = Actor.new("Jennifer Lawrence", 1990, "110000000")
+    movie_1 = Movie.new("Hunger Games", 2012)
+    nominee_1 = Nominee.new(actress_1, movie_1)
+
+
+    actress_2 = Actor.new("Brie Larson", 1989, "10000000")
+    movie_2 = Movie.new("Room", 2015)
+    nominee_2 = Nominee.new(actress_2, movie_2)
+
+    show.add_nominee(nominee_1)
+    show.add_nominee(nominee_2)
+
+    # show.add_nominated_movie
+    # show.add_nominated_movie
+
+    assert_equal [movie_1, movie_2], show.nominated_movies
+  end
+
+  def test_it_can_calculate_average_salary
+    show = AwardShow.new(2018)
+
+    actress_1 = Actor.new("Jennifer Lawrence", 1990, "110000000")
+    movie_1 = Movie.new("Hunger Games", 2012)
+    nominee_1 = Nominee.new(actress_1, movie_1)
+
+
+    actress_2 = Actor.new("Brie Larson", 1989, "10000000")
+    movie_2 = Movie.new("Room", 2015)
+    nominee_2 = Nominee.new(actress_2, movie_2)
+
+    show.add_nominee(nominee_1)
+    show.add_nominee(nominee_2)
+
+    assert_equal 60000000, show.average_salary
+  end 
+
+  # +show.average_salary
+  # +#=> 60000000
 end
